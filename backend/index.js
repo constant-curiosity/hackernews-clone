7cac45f45203b9c81991d17d937d9d4709425398
name <email@email.com>
2023-10-09 02:42:52 +0000

    GraphQL

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+const typeDefs = `
+  type Query {
+    info: String!
+  }
+`;
+const resolvers = {
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`,
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+console.log(`🚀  Server ready at: ${url}`);
