4e5fb59487264d89c6b03ee25ab4ad3d9d7118bf
name <dev.constant@tutanota.com>
2023-12-05 16:16:48 +0000

    working: voting in search feed results

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -38,7 +38,6 @@ const Link = ({
       <div className="flex items-center">
         <span className="gray">{index + 1}.</span>
         <div
-          // className="ml1 gray f11"
           className={`${isVotesIconStyle}`}
           style={{ cursor: "pointer" }}
           onClick={upVote}
@@ -63,42 +62,3 @@ const Link = ({
 };
 
 export default Link;
-
-{
-  // <div className="flex mt2 items-start">
-  //   <div className="flex items-center">
-  //     <span className="gray">{index + 1}.</span>
-  //     <div className="ml1 gray f11" onClick={upvote}>
-  //       ▲
-  //     </div>
-  //   </div>
-  //   <div className="ml1">
-  //   <div className={styles.linkItem}>
-  //     <span className={styles.description}>{description}</span>
-  //     <a href={url} className={styles.url}>
-  //       ({url})
-  //     </a>
-  //     <span className={styles.username}>by {username}</span>
-  //   </div>
-  //     <div className="f6 lh-copy gray">
-  //       {link.votes.length} votes | by{" "}
-  //       {link.postedBy ? link.postedBy.name : "Unknown"}{" "}
-  //       {timeDifferenceForDate(link.createdAt)}
-  //     </div>
-  //   </div>
-  // </div>;
-}
-
-// import styles from "./link.module.css"; // Import the CSS module
-
-// const Link = ({ description, url, username }) => {
-//   return (
-//     <div className={styles.linkItem}>
-//       <span className={styles.description}>{description}</span>
-//       <a href={url} className={styles.url}>
-//         ({url})
-//       </a>
-//       <span className={styles.username}>by {username}</span>
-//     </div>
-//   );
-// };
