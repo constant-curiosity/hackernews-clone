79b0c675a20c7b28e9855486edadca1b45ed8f86
name <dev.constant@tutanota.com>
2023-12-09 21:57:35 +0000

    adding: pagination

--- src/graphql/query/feedList.js
+++ src/graphql/query/feedList.js
@@ -1,8 +1,8 @@
 import gql from "graphql-tag";
 
 export const FEED_QUERY = gql`
-  query Feed($first: Int, $skip: Int, $orderBy: LinkOrderByInput) {
-    feed(first: $first, skip: $skip, orderBy: $orderBy) {
+  query Feed($take: Int, $skip: Int, $orderBy: LinkOrderByInput) {
+    feed(take: $take, skip: $skip, orderBy: $orderBy) {
       links {
         id
         createdAt
